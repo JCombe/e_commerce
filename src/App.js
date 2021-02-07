2a952e9f25acd6d74e2ed5084c243c082e0b8cc0
Julian <biscombe.j@gmail.com>
2021-02-07 12:56:54 +0100

    App, index, commerce, .env

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { commerce } from "./lib/commerce.js"
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Navbar, Products, Cart } from './components';
 
 function App() {
+
+  const [products, setProducts] = useState([])
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+
+  }
+
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+
+  console.log(products)
+
   return (
     <>
       <Router>
